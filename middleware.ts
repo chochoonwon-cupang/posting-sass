652c2fda029f4260fa628a579a87be50c1cdbe24
chochoonwon-cupang <chochoonwon@gmail.com>
2026-02-16 18:39:11 +0900

    narrow middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,10 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    "/dashboard/:path*",
+    "/subscribe/:path*",
+    "/charge/:path*",
+    "/my/:path*",
+    "/settings/:path*",
   ],
 };
